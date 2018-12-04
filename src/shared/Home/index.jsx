afac9f351c41ab4a3a8e34ac1e7b0077b6340529
Jon Preece <jonpreece@rentalcars.com>
2018-12-04 10:46:35 +0000

    Query string override

--- src/shared/Home/index.jsx
+++ src/shared/Home/index.jsx
@@ -1,13 +1,23 @@
 import * as React from 'react'
 
+import withFeatureSwitches from '../withFeatureSwitches'
+import First from '../First'
+import Second from '../Second'
+
 class Home extends React.Component {
   render() {
+    const { features } = this.props
+
     return (
       <>
         <h1>A simple Universal JavaScript feature switch framework for React</h1>
+        <ul>
+          {features.first && <First />}
+          {features.second && <Second />}
+        </ul>
       </>
     )
   }
 }
 
-export default Home
+export default withFeatureSwitches(Home)
